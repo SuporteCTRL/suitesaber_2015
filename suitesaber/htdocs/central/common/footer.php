74371a4b94ab64df8f6c9bb80e4802100eeab22c
suporte <suporte@control.com.br>
2012-02-15 17:42:11 +0000

    Arrumadas as URLS de Suite Saber e da Instituição e incluída a nova variável de URL da Instituição.

--- suitesaber/htdocs/central/common/footer.php
+++ suitesaber/htdocs/central/common/footer.php
@@ -8,8 +8,8 @@
 		<div class="footer">
 
 				<strong><?php echo $def["LEGEND1"]; ?> | versão <?php echo $def["VERSION"] ?></strong><br />
-				<a href="<?php echo $def["URL1"]; ?>" target=_blank><span><?php echo $def["LEGEND2"]; ?></span></a> -
-				<a href="<?php echo $def["URL2"]; ?>" target=_blank><span><?php  echo $institution_name; ?></span></a><br />
+				<a href="<?php echo $def["URL2"]; ?>" target=_blank><span><?php echo $def["LEGEND2"]; ?></span></a> -
+				<a href="<?php echo $institution_url; ?>" target=_blank><span><?php  echo $institution_name; ?></span></a><br />
 
 
 		</div>
