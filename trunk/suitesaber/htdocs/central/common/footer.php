e1a509a25c0f1df42d03fd7cb9699c759159dd51
roger@control.com.br <roger@control.com.br@f346a8c7-21aa-09c2-c95f-b658bc17f081>
2013-02-05 16:47:05 +0000

    Analytics inserido.

--- trunk/suitesaber/htdocs/central/common/footer.php
+++ trunk/suitesaber/htdocs/central/common/footer.php
@@ -9,3 +9,17 @@
 				<a href="<?php echo $def["URL2"]; ?>" target=_blank><span><?php  echo $institution_name; ?></span></a><br />
 		</div>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-13154500-5']);
+  _gaq.push(['_setDomainName', 'suitesaber.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
